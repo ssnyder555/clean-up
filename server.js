a22532f4efa69f12445b6366a5234cc680feb37f
Stephen Snyder <ssnyder555@live.com>
2018-10-24 10:48:59 -0500

    worked on server.js

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
-const express = require('express');
-const app     = express();
 
-
-
-
-
+const express       = require('express');
+const app           = express();
+const bodyParser    = require('body-parser');
+const mthodOverride = require('multer');
+const session       = require('express-session');
 
 // require our database
 require('./db/db');
 
+// require our middle-ware
+const cleanerContollers = require('./controllers/cleanerContollers');
+const clientControllers = require('./controllers/clientControllers');
+// If I get to Auth Controllers
+const authControllers   = require('./controllers/authControllers');
+
+// session has to do with: being loged in for duration of time
+app.use(session ({
+  secret: 'This is some random string',
+  resave: false,
+  saveUnitialized: false
+}));
+
+// This is where I apply my controllers:
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(methodOverride('_method'));
+app.use('/cleaner', cleanerContollers);
+app.use('/client', clientControllers);
+app.use('/auth', authControllers);
 
 
 app.get('/', (req, res) => {
